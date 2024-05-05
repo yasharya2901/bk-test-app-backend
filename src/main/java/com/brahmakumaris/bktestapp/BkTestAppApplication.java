db434f5b060dc082321353dddc639c6b97d2be5b
yasharya2901 <yasharya2901@gmail.com>
2024-05-05 16:15:45 +0530

    Initial Commit: Folder Structure

---
+++ src/main/java/com/brahmakumaris/bktestapp/BkTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.brahmakumaris.bktestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BkTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brahmakumaris/bktestapp/BkTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brahmakumaris.bktestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
