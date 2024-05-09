1f0a0113f7d0121f5ff0476932a887c4add958ac
yasharya2901 <yasharya2901@gmail.com>
2024-05-09 12:50:28 +0530

    Added Question Title attribute and Tag model to better manage questions

---
+++ src/main/java/com/brahmakumaris/bktestapp/models/Tag.java
@@ -0,0 +1,23 @@
+package com.brahmakumaris.bktestapp.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tagId;
+
+    @Column(name = "tag_name")
+    private String tagName;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Question> questions;
+}
